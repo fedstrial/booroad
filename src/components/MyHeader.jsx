45d1c388947fc8fffa1c49a62a6c9520212abc9f
fedstrial <fedstrial@proton.me>
2025-11-07 20:15:42 +0100

    switch text with logo

--- src/components/MyHeader.jsx
+++ src/components/MyHeader.jsx
@@ -1,14 +1,13 @@
 import { useLocation, Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function MyHeader() {
 	const location = useLocation();
 
 	return (
 		<header className="bg-light">
-			<div className="container-wide py-3 d-flex justify-content-between align-items-center">
-				<h1 className="display-1">
-					<Link to="/">BooRoad</Link>
-				</h1>
+			<div className="container-wide py-1 d-flex justify-content-between align-items-center">
+					<Link to="/"><img src={logo} alt="BooRoad" style={{ height: '300px', width: '300px' }} /></Link>
 				{location.pathname === "/users" && (
 					<Link to="/" className="btn btn-primary">
 						Torna alla Homepage
